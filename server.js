4117cfb766e08caad4340fb1ac93b9cea1b4e72d
mandrabi <mandrabi@paradigm.com>
2022-07-06 10:50:27 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/File");
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+const upload = multer({ dest: "uploads" });
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  if ((req.body.password != null) & (req.body.password !== "")) {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+
+  console.log(file);
+  res.send(file.originalName);
+});
+
+app.listen(process.env.PORT);
